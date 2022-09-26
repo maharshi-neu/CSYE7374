149a7f0418801c775c739b9b66aab15519409421
Robin Hillyard <r.hillyard@northeastern.edu>
2022-09-25 20:24:37 -0400

    Added render for Blocks, toString for BlockMessage

--- src/main/scala/crypto/Hash.scala
+++ src/main/scala/crypto/Hash.scala
@@ -1,5 +1,7 @@
 package crypto
 
+import scala.collection.mutable
+
 type Block = Array[Byte]
 
 type CompressionFunction = (Block, Block) => Block
@@ -11,7 +13,13 @@ trait Hash {
 case class BlockHash(bits: Int, cf: CompressionFunction, iv: Block) extends Hash :
     def hash(message: BlockMessage): Block = message.blocks.foldLeft(iv)((r, b) => cf(r, b))
 
-case class BlockMessage(blocks: Iterator[Block])
+case class BlockMessage(blocks: Seq[Block]) {
+    override def toString: String = {
+        val sb = new mutable.StringBuilder()
+        blocks.foreach(b => sb.append(BlockMessage.render(b)))
+        sb.toString
+    }
+}
 
 object BlockMessage {
     def apply(bytes: Int, block: Block): BlockMessage = {
@@ -19,9 +27,11 @@ object BlockMessage {
         BlockMessage(for (b <- rawBlocks) yield pad(bytes, b))
     }
 
+    def apply(blocks: Iterator[Block]): BlockMessage = BlockMessage(blocks.toSeq)
+
     def apply(bytes: Int, message: String): BlockMessage = apply(bytes, message.getBytes())
 
-    val padding: Byte = 'Q'.toByte
+    val padding: Byte = 0.toByte
 
     def pad(bytes: Int, block: Block): Block = if block.length == bytes then block else {
         val result = new Array[Byte](bytes)
@@ -31,10 +41,21 @@ object BlockMessage {
     }
 
     implicit class Xor(block: Block) {
-        def xor(other: Block): Block = {
-            val zipped: Array[(Byte, Byte)] = block zip other
-            val result: Block = for ((b1, b2) <- zipped) yield (b1.toInt ^ b2.toInt).toByte
-            result
-        }
+        /**
+         * Method to XOR block with other.
+         * NOTE: the sizes of the blocks should be the same but, in any case, the length of the output will be that of the shortest block.
+         *
+         * @param other a Block.
+         * @return
+         */
+        infix def xor(other: Block): Block = for ((b1, b2) <- block zip other) yield xor(b1, b2)
+
+        private def xor(b1: Byte, b2: Byte) = (b1.toInt ^ b2.toInt).toByte
+    }
+
+    def render(b: Block): String = {
+        val sb = new mutable.StringBuilder()
+        b.foreach(byte => sb.append("%02x".format(byte)))
+        sb.toString
     }
 }
--- src/test/scala/crypto/HashSpec.scala
+++ src/test/scala/crypto/HashSpec.scala
@@ -3,7 +3,24 @@ package crypto
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should
 
-class BlockMessageSpec extends AnyFlatSpec with should.Matchers {
+//noinspection DeprecatedAlphanumericInfixCall
+class HashSpec extends AnyFlatSpec with should.Matchers {
+
+    behavior of "Hash"
+
+    it should "hash" in {
+        val target = new Hash() {
+            def hash(message: BlockMessage): Block = {
+                import crypto.BlockMessage.Xor
+                def compress(as: Block, bs: Block): Block = as xor bs
+
+                val iv: Block = (LazyList.continually(0xCC) map (_.toByte) take 8).to(Array)
+                message.blocks.foldLeft(iv)((r, b) => compress(r, b))
+            }
+        }
+        val result: Block = target.hash(BlockMessage(8, new Array[Byte](8)))
+        BlockMessage.render(result) shouldBe "cccccccccccccccc"
+    }
 
     behavior of "BlockMessage"
 
@@ -18,12 +35,17 @@ class BlockMessageSpec extends AnyFlatSpec with should.Matchers {
         val result = BlockMessage.pad(20, block)
         result.length shouldBe 20
         result(0) shouldBe 'H'
-        result(19) shouldBe 'Q'
+        result(19) shouldBe 0
     }
 
     it should "apply" in {
         val b: BlockMessage = BlockMessage.apply(16, "The quick brown fox jumps over the lazy dog")
         b.blocks.length shouldBe 3
+    }
+
+    it should "toString" in {
+        val bm = BlockMessage(8, "A")
+        bm.toString shouldBe "4100000000000000"
     }
 
     behavior of "BlockHash"
@@ -32,7 +54,7 @@ class BlockMessageSpec extends AnyFlatSpec with should.Matchers {
         import crypto.BlockMessage.Xor
         val target: BlockHash = BlockHash(128, (r, b) => r.xor(b), new Array[Byte](16))
         val result: Block = target.hash(BlockMessage.apply(16, "The quick brown fox jumps over the lazy dog"))
-        result.foreach(println)
+        BlockMessage.render(result) shouldBe "5a623d6c7a7a7d337c6f6a040a054e54"
     }
 
 }
